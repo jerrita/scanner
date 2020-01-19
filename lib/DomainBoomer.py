8fbbb617fe89b95b756f0a34925302fdef5b97bb
jerrita <782981223@qq.com>
2020-01-19 11:58:25 +0800

    update sub_domain boomer

---
+++ lib/DomainBoomer.py
@@ -0,0 +1,36 @@
+import threading
+import dns.resolver
+
+
+class Boomer(threading.Thread):
+    def __init__(self, sub_queue, domain):
+        threading.Thread.__init__(self)
+        self.sub_queue = sub_queue
+        self.domain = domain
+        self.resolver = dns.resolver.Resolver()
+        self.resolver.nameservers = ['119.29.29.29', '182.254.116.116', '114.114.115.115', '114.114.114.114']
+        self.resolver.timeout = 2
+        self.resolver.lifetime = 2
+        self.count = 0
+        self.timeout = 0
+        self.find = 0
+
+    def run(self):
+        while True:
+            if self.sub_queue.empty():
+                break
+            url = self.sub_queue.get(timeout=0.5)
+            self.count += 1
+            try:
+                a = self.resolver.query(url, 'A')
+                ips = []
+                for i in a.response.answer:
+                    for j in i.items:
+                        if j.rdtype == 1:
+                            ips.append(j.address)
+                print(f'Find: {url} -> {" , ".join(ips)}')
+                self.find += 1
+            except dns.resolver.NXDOMAIN:
+                pass
+            except dns.exception.Timeout:
+                self.timeout += 1
--- scanner.py
+++ scanner.py
@@ -1,34 +1,33 @@
-import dns.resolver
-from concurrent.futures import ThreadPoolExecutor
 import argparse
 import queue
 import time
 from lib.PortScanner import PortScanner, get_port_lists
+from lib.DomainBoomer import Boomer
 
 
-def getrecord(url):
-    try:
-        a = dns.resolver.query(url, 'A')
-        ips = []
-        for i in a.response.answer:
-            for j in i.items:
-                if j.rdtype == 1:
-                    ips.append(j.address)
-        print(f'Find: {url} -> {" , ".join(ips)}')
-    except dns.resolver.NXDOMAIN:
-        pass
-
-
-def domainboom(url, passurl, pool_size=128):
-    fp = open(passurl, 'r')
+def domain_boom(url, dic, thread_num):
+    count = 0
+    find = 0
+    timeout = 0
+    fp = open(dic, 'r')
     print(f'Searching for subdomains in {url}...\nGenerating list...')
-    s_list = []
-    for i in fp.readlines():
-        s_list.append(i.strip() + '.' + url)
+    s_list = queue.Queue()
+    for t in fp.readlines():
+        s_list.put(t.strip() + '.' + url)
+    threads = []
     print('Booming...')
-    with ThreadPoolExecutor(pool_size) as executor:
-        executor.map(getrecord, s_list)
+    for t in range(thread_num):
+        threads.append(Boomer(s_list, url))
+    for thread in threads:
+        thread.start()
+    for thread in threads:
+        thread.join()
+    for thread in threads:
+        count += thread.count
+        timeout += thread.timeout
+        find += thread.find
     fp.close()
+    print(f'[Info] {count} subdomains searched, {find} found, {timeout} timeout')
 
 
 if __name__ == '__main__':
@@ -37,18 +36,18 @@ if __name__ == '__main__':
     parser.add_argument('-p', action='store_true', dest='port', help='Port scan mode')
     parser.add_argument('-d', dest='domain', help='Select the domain or ip')
     parser.add_argument('-m', dest='mode', default='1000', help='Select the port_scan mode (0, 50, 100, 1000)')
-    parser.add_argument('-n', dest='thread_num', default='100', help='Select the thread num(port scanner)')
+    parser.add_argument('-n', dest='thread_num', default='100', help='Select the thread num')
     parser.add_argument('-dic', dest='dic', help='Select the dictionary')
-    parser.add_argument('--pool', default='12', help='Select the size of thread_pool')
     res = parser.parse_args()
     start = time.time()
+
     if res.boom:
         if not res.domain:
             exit('Missing subdomains')
         if not res.dic:
             exit('Missing dictionary')
-        domainboom(res.domain, res.dic, int(res.pool))
-        print('Done.')
+        domain_boom(res.domain, res.dic, int(res.thread_num))
+
     if res.port:
         if not res.domain:
             exit('Missing ip')
@@ -65,5 +64,6 @@ if __name__ == '__main__':
         for thread in thread_list:
             thread.join()
         print(f'{len(port_list)} ports scanned.')
+
     end = time.time()
     print(f'[Use time] {end - start}')
