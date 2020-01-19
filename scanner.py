7a3ac8b124dac12029cf2c82f4b30dcec34974ef
jerrita <782981223@qq.com>
2020-01-19 10:50:28 +0800

    add port scanner

--- scanner.py
+++ scanner.py
@@ -1,7 +1,9 @@
 import dns.resolver
 from concurrent.futures import ThreadPoolExecutor
-import random
 import argparse
+import queue
+import time
+from lib.PortScanner import PortScanner, get_port_lists
 
 
 def getrecord(url):
@@ -24,7 +26,6 @@ def domainboom(url, passurl, pool_size=128):
     for i in fp.readlines():
         s_list.append(i.strip() + '.' + url)
     print('Booming...')
-    random.shuffle(s_list)
     with ThreadPoolExecutor(pool_size) as executor:
         executor.map(getrecord, s_list)
     fp.close()
@@ -33,14 +34,36 @@ def domainboom(url, passurl, pool_size=128):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-b', action='store_true', dest='boom', help='Booming subdomains')
+    parser.add_argument('-p', action='store_true', dest='port', help='Port scan mode')
     parser.add_argument('-d', dest='domain', help='Select the domain or ip')
+    parser.add_argument('-m', dest='mode', default='1000', help='Select the port_scan mode (0, 50, 100, 1000)')
+    parser.add_argument('-n', dest='thread_num', default='100', help='Select the thread num(port scanner)')
     parser.add_argument('-dic', dest='dic', help='Select the dictionary')
-    parser.add_argument('--pool', default=128, help='Select the size of thread_pool')
+    parser.add_argument('--pool', default='12', help='Select the size of thread_pool')
     res = parser.parse_args()
+    start = time.time()
     if res.boom:
         if not res.domain:
             exit('Missing subdomains')
         if not res.dic:
             exit('Missing dictionary')
-        domainboom(res.domain, res.dic, res.pool)
+        domainboom(res.domain, res.dic, int(res.pool))
         print('Done.')
+    if res.port:
+        if not res.domain:
+            exit('Missing ip')
+        port_scanner = PortScanner()
+        port_list = get_port_lists(int(res.mode))
+        port_queue = queue.Queue()
+        thread_list = []
+        for port in port_list:
+            port_queue.put(port)
+        for i in range(int(res.thread_num)):
+            thread_list.append(port_scanner.PortScan(port_queue, res.domain))
+        for thread in thread_list:
+            thread.start()
+        for thread in thread_list:
+            thread.join()
+        print(f'{len(port_list)} ports scanned.')
+    end = time.time()
+    print(f'[Use time] {end - start}')
